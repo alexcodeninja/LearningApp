bfdd47521c84415487c1ec4ef69d2a2807579831
alexcodeninja <aalexdeada@gmail.com>
2023-11-06 17:10:30 +0200

    added:  delete button(works):   deletes last order in list  order button(stub - just toast message)
    changed:
     theme colors(purple -> green)
     MainActivity.kt:
      everything works with bindings
     order_card.xml:
      now have the spacing
    
    and other small changes

--- app/src/main/java/com/example/learningapp/MainActivity.kt
+++ app/src/main/java/com/example/learningapp/MainActivity.kt
@@ -1,31 +1,49 @@
 package com.example.learningapp
-
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import android.widget.EditText
 import android.widget.Space
+import android.widget.Toast
 import androidx.cardview.widget.CardView
 import com.example.learningapp.databinding.ActivityMainBinding
+import com.example.learningapp.databinding.OrderCardBinding
 
 class MainActivity : AppCompatActivity() {
 	private lateinit var binding: ActivityMainBinding
 	private lateinit var orders: MutableList<View>
+
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		binding = ActivityMainBinding.inflate(layoutInflater)
 		setContentView(binding.root)
 
 		orders = mutableListOf()
-		binding.fabAdd.setOnClickListener{
-			val view = layoutInflater.inflate(R.layout.order_card, null) // вопрос по inflater / binding
-			val food: EditText = view.findViewById(R.id.et_food) // ?
-			val count: EditText = view.findViewById(R.id.etn_count) // ?
-			orders.add(view)
-			orders.add(Space(this))
-			binding.vLayOrders.addView(view)
+		binding.fabAdd.setOnClickListener {
+			val card_binding: OrderCardBinding = OrderCardBinding.inflate(layoutInflater)
+//			val food: EditText = card_binding.etFood
+//			val count: EditText = card_binding.etnCount
+			card_binding.twSerialNum.text = (orders.size + 1).toString() + "."
+			orders.add(card_binding.root)
+			binding.vLayOrders.addView(card_binding.root)
 		}
 
+		binding.fabRemove.setOnClickListener{
+			print(orders.size)
+			if (orders.size == 0){
+				Toast.makeText(applicationContext, "Нет заказов для удаления", Toast.LENGTH_SHORT).show()
+			}
+			else {
+				binding.vLayOrders.removeView(orders.last())
+				orders.removeLast()
+			}
+		}
+
+		binding.btnOrder.setOnClickListener{
+			Toast.makeText(applicationContext, orders.joinToString(), Toast.LENGTH_LONG).show()
+		}
 
 
 	}
